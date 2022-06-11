3b9318ae6c0b66776aa6c04311f82d1950f01ba4
hey880 <hey880@naver.com>
2022-06-11 19:24:38 +0900

    delete id from AuthCredentialDto

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -2,9 +2,6 @@ import { IsEmpty } from "class-validator";
 import { PrimaryColumn } from "typeorm";
 
 export class AuthCredentialDto {
-    @PrimaryColumn()
-    id: number;
-    
     @IsEmpty()
     username: string;
 
