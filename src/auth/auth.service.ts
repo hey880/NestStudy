3a7fb56cdca1d9fee32feba28add0b1d2db7151a
hey880 <hey880@naver.com>
2022-07-13 22:12:21 +0900

    feat: Add login service and handler

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { UserRepository } from './user.repository';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -12,5 +13,18 @@ export class AuthService {
 
     async signUp(authCredentialDto:AuthCredentialDto):Promise<void> {
         return this.userRepository.createUser(authCredentialDto);
+    }
+
+    async signIn(authCredentialDto: AuthCredentialDto): Promise<string> {
+        //loging success 라는 메세지 return할 거라서 string으로 타입 줌
+        const { username, password } = authCredentialDto;
+        const user = await this.userRepository.findOne({ username });
+
+        //입력한 password값과 db의 user가 가진 password를 비교
+        if(user && (await bcrypt.compare(password, user.password))) {
+            return 'login success';
+        } else {
+            throw new UnauthorizedException('login failed');
+        }
     }
 }
